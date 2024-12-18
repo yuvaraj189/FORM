5a53e77f2eadbe9a063d30dbc9bea76e63c23820
yuvaraj189 <yuvarajkumar093@gmail.com>
2024-12-18 07:45:13 +0530

    updated

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
+    
     <footer className="bg-indigo-600 text-white py-4 mt-8">
       <div className="max-w-7xl mx-auto px-4 text-center">
-        <p>&copy; 2024 Employee Management System. All rights reserved.</p>
+        <p>&copy; 2024 Employee Management System. All rights reserved @yuva.</p>
       </div>
     </footer>
   );
